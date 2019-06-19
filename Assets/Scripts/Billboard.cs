a2c487bf44f6556de8ebdb146f9c82387f28a28f
Dakota Crouchelli <earthquakedriver@protonmail.com>
2019-06-18 21:29:00 -0400

    Palm trees, cannon, booze

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Transform player = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player == null)
+            player = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        Vector3 rot = transform.localEulerAngles;
+
+        rot.y = player.transform.localEulerAngles.y;
+        
+        transform.localEulerAngles = rot;
+    }
+}
