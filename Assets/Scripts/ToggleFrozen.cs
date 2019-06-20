e713f4472353d456ed0fc24e86c9c3650aee69e3
Dakota Crouchelli <earthquakedriver@protonmail.com>
2019-06-20 01:52:21 -0400

    Fuckup conversation

---
+++ Assets/Scripts/ToggleFrozen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+[RequireComponent(typeof(RigidbodyFirstPersonController))]
+public class ToggleFrozen : MonoBehaviour
+{
+    private RigidbodyFirstPersonController controller;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<RigidbodyFirstPersonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Freeze()
+    {
+        controller.enabled = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void UnFreeze()
+    {
+        controller.enabled = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
